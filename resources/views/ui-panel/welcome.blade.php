f48dfd6cd69d9121b87c1e9a8af3340e5c429f1b
Phyo Lay <juniorphyolay10@gmail.com>
2024-01-03 10:54:20 +0630

    ninth

--- resources/views/ui-panel/welcome.blade.php
+++ resources/views/ui-panel/welcome.blade.php
@@ -10,8 +10,21 @@
             </div>
         </div>
     </div>
-    <div class="container my-5">
-        <div class="row">
+    <div class="container my-5 order-md-1">
+        <div class="row">           
+            <div class="col-md-4 order-md-2 mb-5">
+                <h5 class="ms-4 mb-4 fw-bold">Category</h5>
+                <ul style="list-style: none">
+                    <li class="mb-2">
+                        <a href="{{ route('searchById') }}" class="fw-bold">All</a>
+                    </li>
+                    @foreach ($categories as $category)
+                    <li class="mb-2">
+                        <a href="{{route('searchById' , $category->id)}}" class="fw-bold">{{$category->name}}</a>
+                    </li>
+                    @endforeach
+                </ul>
+            </div>
             <div class="col-md-8">
                 @if ($jobs->isEmpty())
                 <p class="mt-3 fw-bold fs-5">No jobs available for this category.</p>
@@ -33,20 +46,6 @@
                 </div>
                 @endforeach  
                 @endif
-            </div>
-
-            <div class="col-md-4">
-                <h5 class="ms-4 mb-4 fw-bold">Category</h5>
-                <ul style="list-style: none">
-                    <li class="mb-2">
-                        <a href="{{ route('searchById') }}" class="fw-bold">All</a>
-                    </li>
-                    @foreach ($categories as $category)
-                    <li class="mb-2">
-                        <a href="{{route('searchById' , $category->id)}}" class="fw-bold">{{$category->name}}</a>
-                    </li>
-                    @endforeach
-                </ul>
             </div>
         </div>
     </div> 
